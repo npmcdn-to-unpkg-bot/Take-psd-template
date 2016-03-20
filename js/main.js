12accb26008b5623ef487136d74457e39b1817ee
VladyslavDmitriyev <vladyslav.dmitriyev@gmail.com>
2016-03-21 01:14:03 +0200

    masonry grid - fixed

--- js/main.js
+++ js/main.js
@@ -1,14 +1,19 @@
 
 (function ($, undefind) {
+
+    $(".grid-item").addClass(".animated .rotateIn");
     
-var $container = $(".masonry-container");
-	$container.imagesLoaded(function () {
-		$container.masonry({
-            itemSelector: ".grid-item",
-            percentPosition: true,
-            columnWidth: '.grid-sizer'
-            });
-});
+    var $container = $(".masonry-container");
+        $container.imagesLoaded(function () {
+            $container.masonry({
+                itemSelector: ".grid-item",
+                percentPosition: true,
+                columnWidth: '.grid-item'
+                });
+            
+    });
+    
+
     
 })(jQuery);
 
